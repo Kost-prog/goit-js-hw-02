7fa6b89309485b5285a100518c5729755adb5f90
Kost-prog <kostyuk.mila@gmail.com>
2025-02-01 21:39:04 +0200

    change 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,16 +1,19 @@
 function getShippingCost(country) {
-    switch (country)
-    {
-      case country = "China" : return `Shipping to ${country} will cost 100 credits`;
-        break;
-      case country = "Chile" : return `Shipping to ${country} will cost 250 credits`;
-        break;
-      case country = "Australia" : return `Shipping to ${country} will cost 170 credits`;
-            break;
-      case country = "Jamaica" : return `Shipping to ${country} will cost 120 credits`;
-            break;
-      default: return "Sorry, there is no delivery to your country"
-    }
+  let price;
+  switch (country) {
+    case "China": price = 100;
+      break;
+    case "Chile": price = 250;
+      break;
+    case "Australia": price = 170;
+      break;
+    case "Jamaica": price = 120;
+      break;
+    default:
+      return "Sorry, there is no delivery to your country";
+  }
+
+  return `Shipping to ${country} will cost ${price} credits`;
 }
 console.log(getShippingCost("Australia"));
 console.log(getShippingCost("Germany"));
